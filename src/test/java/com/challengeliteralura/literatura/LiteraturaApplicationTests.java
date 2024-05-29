6700135ede176e17e8757248ade6342d2efbe53a
MamaniMarioOrlando <46626380+MamaniMarioOrlando@users.noreply.github.com>
2024-05-29 10:18:37 -0300

    Inicio del challenge

---
+++ src/test/java/com/challengeliteralura/literatura/LiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeliteralura.literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
